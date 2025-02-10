15f68e1c90577767409a6d0448ff70b053501394
Zone.N <zone.niuzh@hotmail.com>
2025-02-10 17:08:53 +0800

    init
    Signed-off-by: Zone.N <zone.niuzh@hotmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # smcc
 C functions and constants for Arm SMCCC and PSCI
+
+Ref: https://github.com/google/smccc
---
+++ include/smcc.h
@@ -0,0 +1,22 @@
+
+/**
+ * @file smcc.h
+ * @brief smcc 头文件
+ * @author Zone.N (Zone.Niuzh@hotmail.com)
+ * @version 1.0
+ * @date 2025-01-02
+ * @copyright MIT LICENSE
+ * https://github.com/MRNIU/smcc
+ * @par change log:
+ * <table>
+ * <tr><th>Date<th>Author<th>Description
+ * <tr><td>2025-01-02<td>Zone.N (Zone.Niuzh@hotmail.com)<td>创建文件
+ * </table>
+ */
+
+#ifndef SMCC_INCLUDE_SMCC_H_
+#define SMCC_INCLUDE_SMCC_H_
+
+
+
+#endif /* SMCC_INCLUDE_SMCC_H_ */
