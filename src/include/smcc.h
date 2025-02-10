9bd16d113ef7204fcde31b6e97c3113cbe7f81a7
Zone.N <zone.niuzh@hotmail.com>
2025-02-10 17:15:04 +0800

    init
    Signed-off-by: Zone.N <zone.niuzh@hotmail.com>

--- src/include/smcc.h
+++ src/include/smcc.h
@@ -4,19 +4,20 @@
  * @brief smcc 头文件
  * @author Zone.N (Zone.Niuzh@hotmail.com)
  * @version 1.0
- * @date 2025-01-02
+ * @date 2024-05-24
  * @copyright MIT LICENSE
  * https://github.com/MRNIU/smcc
  * @par change log:
  * <table>
  * <tr><th>Date<th>Author<th>Description
- * <tr><td>2025-01-02<td>Zone.N (Zone.Niuzh@hotmail.com)<td>创建文件
+ * <tr><td>2024-05-24<td>Zone.N (Zone.Niuzh@hotmail.com)<td>创建文件
  * </table>
  */
 
-#ifndef SMCC_INCLUDE_SMCC_H_
-#define SMCC_INCLUDE_SMCC_H_
-
-
-
-#endif /* SMCC_INCLUDE_SMCC_H_ */
+ #ifndef SMCC_SRC_INCLUDE_SMCC_H_
+ #define SMCC_SRC_INCLUDE_SMCC_H_
+ 
+ 
+ 
+ #endif /* SMCC_SRC_INCLUDE_SMCC_H_ */
+ 
---
+++ src/smcc.c
@@ -0,0 +1,17 @@
+
+/**
+ * @file smcc.c
+ * @brief smcc 实现
+ * @author Zone.N (Zone.Niuzh@hotmail.com)
+ * @version 1.0
+ * @date 2024-05-24
+ * @copyright MIT LICENSE
+ * https://github.com/MRNIU/smcc
+ * @par change log:
+ * <table>
+ * <tr><th>Date<th>Author<th>Description
+ * <tr><td>2024-05-24<td>Zone.N (Zone.Niuzh@hotmail.com)<td>创建文件
+ * </table>
+ */
+
+#include "smcc.h"
